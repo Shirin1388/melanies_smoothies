5b1173561a4edbf793251342914bf16b4b65a558
Shirin1388 <iraji.shirin@gmail.com>
2025-10-16 13:02:01 +0800

    Update streamlit_app.py
    Adding Search_ON column for searching

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
 import streamlit as st
 import requests
-# from snowflake.snowpark.context import get_active_session
+import pandas as pd
 from snowflake.snowpark.functions import col
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 
@@ -13,8 +14,8 @@ st.write(
   """
 )
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be ", name_on_order)
@@ -32,8 +33,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -47,5 +49,3 @@ if ingredients_list:
         if ingredients_string:
             session.sql(my_insert_stmt).collect()
             st.success('Your Smoothie is ordered! ' + name_on_order, icon="✅")
-
-
